fbc9b2aa55a82489007df5bf9c0ce41ec94e3b44
tonyli8821 <76194931+tonyli8821@users.noreply.github.com>
2020-12-27 17:39:30 +0800

    Create Compare.java

---
+++ java/Compare.java
@@ -0,0 +1,12 @@
+public class Compare{
+  public static main(String[] args){
+    String c1 = new String("abc");
+    String c2 = new String("abc");
+    String c3 = c1;
+    
+    System.out.println("the result c2 == c3 : " + (c2 == c3));
+    
+    System.out.println("c2.equals(c3) : " +(c2.equals(c3)));
+    
+    }
+ }
